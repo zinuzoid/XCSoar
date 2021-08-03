cc3277d83e79fc2e955b164aaef2d8af6745cbf8
Jetsada Machom <zinuzoid@gmail.com>
2021-08-03 02:08:20 +0100

    [Skysight] convert NetCDF to GeoTIFF
    Original commit from: https://github.com/CazYokoyama/XCSoar

--- build/thirdparty.py
+++ build/thirdparty.py
@@ -145,6 +145,8 @@ elif 'apple-darwin' in actual_host_triplet:
 elif 'android' in actual_host_triplet:
     thirdparty_libs = [
         libfmt,
+        netcdf,
+        netcdfcxx,
         libsodium,
         openssl,
         cares,
