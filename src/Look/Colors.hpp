e444edad4096759d19e3f3ff7cecea17410633ce
Jetsada Machom <zinuzoid@gmail.com>
2021-10-04 01:33:33 +0100

    Update brand asset, color

--- src/Look/Colors.hpp
+++ src/Look/Colors.hpp
@@ -26,9 +26,10 @@ Copyright_License {
 #include "ui/canvas/Color.hpp"
 
 #ifdef XCSOAR_TESTING
-static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xed, 0x90, 0x90);
-static constexpr Color COLOR_XCSOAR = Color(0xd0, 0x17, 0x17);
-static constexpr Color COLOR_XCSOAR_DARK = Color(0x5d, 0x0a, 0x0a);
+static constexpr Color COLOR_XCSOAR_LIGHT = Color(0x57, 0xff, 0xd8);
+static constexpr Color COLOR_XCSOAR = Color(0x00, 0xf0, 0xb8);
+static constexpr Color COLOR_XCSOAR_DARK = Color(0x00, 0xa3, 0x7d);
+// New palette 00f0b8,ff3d51,ffe97a,004be0,8d85ff
 #else
 static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xaa, 0xc9, 0xe4);
 static constexpr Color COLOR_XCSOAR = Color(0x3f, 0x76, 0xa8);
