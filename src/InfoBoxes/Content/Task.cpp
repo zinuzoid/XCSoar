9a83a29c92f5cfa310f9d14dc52ed5317e862e4d
Jetsada Machom <zinuzoid@gmail.com>
2022-02-19 16:27:34 -0800

    [Waypoint] Show waypoint shortcode

--- src/InfoBoxes/Content/Task.cpp
+++ src/InfoBoxes/Content/Task.cpp
@@ -164,7 +164,15 @@ UpdateInfoBoxNextDistance(InfoBoxData &data) noexcept
   if (!way_point)
     data.SetTitle(_("WP Dist"));
   else
-    data.SetTitle(way_point->name.c_str());
+    if(way_point->shortname.length() > 0) {
+      StaticString<100> buffer;
+      buffer.Format(("[%s] %s"),
+                    way_point->shortname.c_str(),
+                    way_point->name.c_str());
+      data.SetTitle(buffer);
+    } else {
+      data.SetTitle(way_point->name.c_str());
+    }
 
   // use proper non-terminal next task stats
 
@@ -899,7 +907,15 @@ InfoBoxContentNextArrow::Update(InfoBoxData &data) noexcept
   if (!way_point)
     data.SetTitle(_("Next arrow"));
   else
-    data.SetTitle(way_point->name.c_str());
+    if(way_point->shortname.length() > 0) {
+      StaticString<100> buffer;
+      buffer.Format("[%s] %s",
+              way_point->shortname.c_str(),
+              way_point->name.c_str());
+      data.SetTitle(buffer);
+    } else {
+      data.SetTitle(way_point->name.c_str());
+    }
 
   // Set value
   if (angle_valid)
