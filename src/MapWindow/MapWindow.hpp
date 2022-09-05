37f39c29614ad3a90a77fddb8bec7045efae5ea0
Jetsada Machom <zinuzoid@gmail.com>
2022-09-05 14:24:31 -0700

    [Skysight] Weather overlay rendering
    Original commit from: https://github.com/CazYokoyama/XCSoar

--- src/MapWindow/MapWindow.hpp
+++ src/MapWindow/MapWindow.hpp
@@ -359,6 +359,8 @@ private:
 
   void RenderRasp(Canvas &canvas) noexcept;
 
+  void RenderSkysight(Canvas &canvas) noexcept;
+
   void RenderTerrainAbove(Canvas &canvas, bool working) noexcept;
 
   /**
