c08f5a7881ff82a0e2b3b216c6f751b8b0f69555
Jetsada Machom <zinuzoid@gmail.com>
2024-05-30 16:56:52 +0100

    AudioTaskBearing

--- src/Audio/VarioSynthesiser.cpp
+++ src/Audio/VarioSynthesiser.cpp
@@ -7,11 +7,6 @@
 #include <algorithm>
 #include <cassert>
 
-/**
- * The minimum and maximum vario range for the constants below [cm/s].
- */
-static constexpr int min_vario = -500, max_vario = 500;
-
 unsigned
 VarioSynthesiser::VarioToFrequency(int ivario)
 {
