2612079a257c9e1761a31914ab0bff042c6122bb
Jetsada Machom <zinuzoid@gmail.com>
2024-06-10 20:56:32 +0100

    [Android] Remove screen immersion feature and show status bar by default

--- android/src/WindowUtil.java
+++ android/src/WindowUtil.java
@@ -27,27 +27,14 @@ class WindowUtil {
    * Screen Mode.
    */
   static void enableImmersiveMode(Window window) {
-    View decorView = window.getDecorView();
-    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN|
-                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
-                                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
-                                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
-                                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
-                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
   }
 
   static void disableImmersiveMode(Window window) {
-    View decorView = window.getDecorView();
-    decorView.setSystemUiVisibility(0);
   }
 
   static void enterFullScreenMode(Window window) {
-    window.addFlags(FULL_SCREEN_WINDOW_FLAGS);
-    enableImmersiveMode(window);
   }
 
   static void leaveFullScreenMode(Window window, int preserveFlags) {
-    disableImmersiveMode(window);
-    window.clearFlags(FULL_SCREEN_WINDOW_FLAGS & ~preserveFlags);
   }
 }
