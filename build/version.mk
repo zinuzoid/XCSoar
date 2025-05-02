68f0754a478e8475d1353704e86652398a30f785
Jetsada Machom <zinuzoid@gmail.com>
2025-05-02 12:24:46 +0100

    [build] Always append git commit in version string

--- build/version.mk
+++ build/version.mk
@@ -5,11 +5,8 @@ VERSION_CPPFLAGS = -DXCSOAR_VERSION=\"$(VERSION)\"
 
 GIT_COMMIT_ID := $(shell git rev-parse --short --verify HEAD 2>$(NUL))
 RELEASE_COMMIT_ID := $(shell git rev-parse --short --verify "v$(VERSION)^{commit}" 2>$(NUL))
-# only append the commit id for unreleased builds (no release tag)
-ifneq ($(GIT_COMMIT_ID),$(RELEASE_COMMIT_ID))
 VERSION_CPPFLAGS += -DGIT_COMMIT_ID=\"$(GIT_COMMIT_ID)\"
 FULL_VERSION := $(FULL_VERSION)~$(GIT_COMMIT_ID)
-endif
 
 $(call SRC_TO_OBJ,$(SRC)/Version.cpp): $(topdir)/VERSION.txt
 $(call SRC_TO_OBJ,$(SRC)/Version.cpp): CPPFLAGS += $(VERSION_CPPFLAGS)
