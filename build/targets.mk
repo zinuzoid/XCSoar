70072463e87b764836437547e27e9c00f41fdd16
Jetsada Machom <zinuzoid@gmail.com>
2025-09-15 17:20:10 +0100

    [Android] increase targetSdkVersion to 35 and minimum to 23

--- build/targets.mk
+++ build/targets.mk
@@ -315,7 +315,7 @@ endif
 ifeq ($(TARGET),ANDROID)
   ANDROID_NDK ?= $(HOME)/opt/android-ndk-r27c
 
-  ANDROID_SDK_PLATFORM = android-34
+  ANDROID_SDK_PLATFORM = android-35
   ANDROID_NDK_API = 27
 
   # The naming of CPU ABIs, architectures, and various NDK directory names is an unholy mess.
