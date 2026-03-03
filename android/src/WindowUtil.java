e45b79b97ab0c7c1e1f70f7a434b59f52ebf83e6
Jetsada Machom <zinuzoid@gmail.com>
2026-03-03 22:58:38 +0100

    [Android] support immersive mode

--- android/src/WindowUtil.java
+++ android/src/WindowUtil.java
@@ -3,9 +3,12 @@
 
 package org.xcsoar;
 
-import android.view.Window;
-import android.view.WindowManager;
+import android.os.Build;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 
 /**
  * A library of utility functions for class #Window.
@@ -27,14 +30,46 @@ class WindowUtil {
    * Screen Mode.
    */
   static void enableImmersiveMode(Window window) {
+    if (Build.VERSION.SDK_INT >= 30) {
+      WindowInsetsController controller = window.getInsetsController();
+      if (controller != null) {
+        controller.hide(WindowInsets.Type.systemBars());
+        controller.setSystemBarsBehavior(
+          WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+      }
+    } else {
+      window.getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_FULLSCREEN |
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
   }
 
   static void disableImmersiveMode(Window window) {
+    if (Build.VERSION.SDK_INT >= 30) {
+      WindowInsetsController controller = window.getInsetsController();
+      if (controller != null) {
+        controller.show(WindowInsets.Type.systemBars());
+      }
+    } else {
+      window.getDecorView().setSystemUiVisibility(0);
+    }
   }
 
   static void enterFullScreenMode(Window window) {
+    if (Build.VERSION.SDK_INT < 35)
+      window.addFlags(FULL_SCREEN_WINDOW_FLAGS);
+
+    enableImmersiveMode(window);
   }
 
   static void leaveFullScreenMode(Window window, int preserveFlags) {
+    disableImmersiveMode(window);
+
+    if (Build.VERSION.SDK_INT < 35)
+      window.clearFlags(FULL_SCREEN_WINDOW_FLAGS & ~preserveFlags);
   }
 }
